fdecd6071bbf4007f82441215bbdb06c16042d45
shakil1819 <shakilmrf8@gmail.com>
2024-07-14 12:20:42 +0600

    Added FTP configurations

--- ftp_processor.py
+++ ftp_processor.py
@@ -6,3 +6,13 @@ import threading
 import xml.etree.ElementTree as ET
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+
+FTP_HOST = "localhost"
+FTP_USER = "nybsys"
+FTP_PASS = "12345"
+
+
+TEMP_DIR = "./TEMP"
+LOCAL_DIR = "./LOCAL"
+TRASH_DIR = "./TRASH"
